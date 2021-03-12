a2df1f4c43992124a64669195599eef9f51bea50
robbyyt <49028058+robbyyt@users.noreply.github.com>
2021-03-12 15:55:26 +0200

    bbc and cnn

---
+++ scrapers/bbc.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup as bs
+from langdetect import detect
+
+
+class BBC:
+    def __init__(self, url: str):
+        article = requests.get(url)
+        self.soup = bs(article.content, 'html.parser')
+        self.body = self.get_body()
+        self.title = self.get_title()
+        self.language = self.get_language()
+
+    def get_body(self) -> list:
+        article_elem = self.soup.find('article')
+        body = article_elem.find_all('div', {'data-component': 'text-block'})
+        content = []
+        for div in body:
+            content.append(
+                div.find('div').find('p').text
+            )
+        return content
+
+    def get_title(self) -> str:
+        return self.soup.find('h1').text
+
+    def get_language(self) -> str:
+        try:
+            return self.soup.find('html')['lang']
+        except:
+            return detect(self.title)
+
+    def to_object(self):
+        return {
+            'title': self.title,
+            'body': self.body,
+            'language': self.language
+        }
+
+
+if __name__ == '__main__':
+    article = BBC('https://www.bbc.com/news/world-us-canada-56368328')
+    print(article.to_object())
---
+++ scrapers/cnn.py
@@ -0,0 +1,46 @@
+import requests
+from bs4 import BeautifulSoup as bs
+from langdetect import detect
+
+
+class CNN:
+    def __init__(self, url: str):
+        article = requests.get(url)
+        self.soup = bs(article.content, 'html.parser')
+        self.body = self.get_body()
+        self.title = self.get_title()
+        self.language = self.get_language()
+
+    def get_body(self) -> list:
+        headline = self.soup.find_all('p', {'class': 'zn-body__paragraph'})
+        headline_text = ''
+        for tag in headline:
+            headline_text += tag.text
+        content = [headline_text]
+        text_divs = self.soup.find_all('div', {'class': 'zn-body__paragraph'})
+
+        for div in text_divs:
+            content.append(div.text)
+
+        return content
+
+    def get_title(self) -> str:
+        return self.soup.find('h1').text
+
+    def get_language(self) -> str:
+        try:
+            return self.soup.find('html')['lang']
+        except:
+            return detect(self.title)
+
+    def to_object(self):
+        return {
+            'title': self.title,
+            'body': self.body,
+            'language': self.language
+        }
+
+
+if __name__ == '__main__':
+    article = CNN('https://edition.cnn.com/2021/03/11/americas/brazil-variants-simultaneous-infection-intl/index.html')
+    print(article.to_object())
